fda75da672257fb2b2974658e037b4da3510c7da
Diego Rojas <rrojas1296@gmail.com>
2025-08-23 18:52:30 -0500

    master: Changed host

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   });
   app.use(cookieParser.default());
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
